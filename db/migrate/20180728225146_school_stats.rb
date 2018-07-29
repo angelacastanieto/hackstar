be797d9724d74c6b04106a16c61cce4591eb3e92
angelacastanieto <angela@gametime.co>
2018-07-29 10:18:24 -0700

    fix data model

--- db/migrate/20180728225146_school_stats.rb
+++ db/migrate/20180728225146_school_stats.rb
@@ -1,23 +1,21 @@
 class SchoolStats < ActiveRecord::Migration[5.2]
   def change
     create_table :school_stats do |t|
-      t.string :lat, null: false, default: ""
-      t.string :lon, null: false, default: ""
       t.string :name, null: false, default: ""
-      t.string :address, null: false, default: ""
+      t.string :num, null: false, default: ""
       t.string :grade, null: false, default: ""
-      t.boolean :transportation_provided, null: false, default: false
-      t.string :language_immersion, null: false, default: ""
-    end
-
-    create_table :school_stats_2 do |t|
-      t.string :lat, null: false, default: ""
-      t.string :lon, null: false, default: ""
-      t.string :name, null: false, default: ""
-      t.string :address, null: false, default: ""
-      t.string :grade, null: false, default: ""
-      t.boolean :transportation_provided, null: false, default: false
-      t.string :language_immersion, null: false, default: ""
+      t.string :b_cantonese, null: false, default: ""
+      t.string :b_spanish, null: false, default: ""
+      t.string :i_cantonese, null: false, default: ""
+      t.string :i_korean, null: false, default: ""
+      t.string :i_mandarin, null: false, default: ""
+      t.string :i_spanish, null: false, default: ""
+      t.string :f_filipino, null: false, default: ""
+      t.string :f_japanese, null: false, default: ""
+      t.string :n_chinese, null: false, default: ""
+      t.string :middle, null: false, default: ""
+      t.int :start_time, null: true
+      t.string :zipcode, null: true
     end
   end
 end
