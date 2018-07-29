0587016b6f3c07ad68e8e54b8a55721925fc378a
angelacastanieto <angela@gametime.co>
2018-07-29 11:18:05 -0700

    query postgres for schools

--- app.rb
+++ app.rb
@@ -9,39 +9,41 @@ class App < Sinatra::Base
   end
 
   get "/pdf" do
-    File.delete('form.pdf') if File.exist?('form.pdf')
-
     pdf = FillablePDF.new 'final_form.pdf'
     path = params['path']
 
+
+    File.delete('form.pdf') if File.exist?('form.pdf')
     c1 = params['code1']
     c2 = params['code2']
     c3 = params['code3']
     c4 = params['code4']
     c5 = params['code5']
 
-    s1 = params['school1']
-    s2 = params['school2']
-    s3 = params['school3']
-    s4 = params['school4']
-    s5 = params['school5']
+    code_arr = ["'#{c1}'","'#{c2}'","'#{c3}'","'#{c4}'","'#{c5}'"]
 
+    code_arr_clean = code_arr.reject { |c| c == "''" }
 
-    pdf.set_field(:untitled1, path)
-    pdf.set_field(:untitled2, s1)
-    pdf.set_field(:untitled3, c1)
-    pdf.set_field(:untitled4, path)
-    pdf.set_field(:untitled5, s2)
-    pdf.set_field(:untitled6, c2)
-    pdf.set_field(:untitled7, path)
-    pdf.set_field(:untitled8, s3)
-    pdf.set_field(:untitled9, c3)
-    pdf.set_field(:untitled10, path)
-    pdf.set_field(:untitled11, s4)
-    pdf.set_field(:untitled12, c4)
-    pdf.set_field(:untitled13, path)
-    pdf.set_field(:untitled14, s5)
-    pdf.set_field(:untitled15, c5)
+    conn = PG.connect( dbname: 'hh_proj' )
+
+    counter = 1
+
+    conn.exec("SELECT * from school_form_stats where num in (#{code_arr_clean.join(',')})") do |result|
+      result.each_with_index do |row, i|
+        field_name_1 = "untitled#{counter}"
+        field_name_2 = "untitled#{counter+1}"
+        field_name_3 = "untitled#{counter+2}"
+
+        counter += 3
+
+        name, grade, num = row.values_at('name', 'grade', 'num')
+
+        pdf.set_field(field_name_1.to_sym, grade)
+        pdf.set_field(field_name_2.to_sym, name)
+        pdf.set_field(field_name_3.to_sym, num)
+      end
+    end
+
     pdf.save_as('form.pdf')
 
     headers "Content-Disposition" => "attachment", "Content-Type" => "application/pdf"
